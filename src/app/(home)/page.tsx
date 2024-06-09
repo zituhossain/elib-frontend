458766a842baaa36ced8ac6bc2aada9e3aa04ec1
zituhossain5 <zituhossain@gmail.com>
2024-06-09 21:24:21 +0600

    add cache no store

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,9 @@ import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, {
+    cache: "no-store",
+  });
   if (!response.ok) {
     throw new Error("An error occurred while fetching the data.");
   }

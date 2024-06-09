fa6097984ec952b05c684d3b1e9f9ac0ae971a24
zituhossain5 <zituhossain@gmail.com>
2024-06-09 14:10:23 +0600

    add download button

--- src/app/book/[bookId]/page.tsx
+++ src/app/book/[bookId]/page.tsx
@@ -1,6 +1,7 @@
 import { Book } from "@/types";
 import Image from "next/image";
 import React from "react";
+import DownloadButton from "./components/DownloadButton";
 
 const SingleBookPage = async ({ params }: { params: { bookId: string } }) => {
   console.log("params", params);
@@ -28,7 +29,7 @@ const SingleBookPage = async ({ params }: { params: { bookId: string } }) => {
         <h2 className="mb-5 text-5xl font-bold leading-[1.1]">{book.title}</h2>
         <span className="font-semibold">by {book.author.name}</span>
         <p className="mt-5 text-lg leading-8">{book.description}</p>
-        {/* <DownloadButton fileLink={book.file} /> */}
+        <DownloadButton fileLink={book.file} />
       </div>
       <div className="flex justify-end">
         <Image

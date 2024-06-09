17b6d42e7c0e3bf02fd4e10461aadde3676be8c1
zituhossain5 <zituhossain@gmail.com>
2024-06-09 21:49:01 +0600

    add suspense

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -2,10 +2,21 @@ import { Book } from "@/types";
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = ({ books }: { books: Book[] }) => {
+const BookList = async () => {
+  // data fetching
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, {
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the data.");
+  }
+
+  const books = await response.json();
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto mb-10">
-      {books.map((book) => (
+      {books.map((book: Book) => (
         <BookCard key={book._id} book={book} />
       ))}
     </div>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,15 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_URL}/books`, {
-    cache: "no-store",
-  });
-  if (!response.ok) {
-    throw new Error("An error occurred while fetching the data.");
-  }
-  const books = await response.json();
-  console.log("books", books);
-
   return (
     <>
       <Banner />
-      <BookList books={books} />
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
